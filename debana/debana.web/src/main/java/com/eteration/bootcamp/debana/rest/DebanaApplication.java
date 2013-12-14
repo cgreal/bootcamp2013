d1ad59e76b2957808fa3f6743dbefb8877c82819
mervekara <mervekara3037@gmail.com>
2013-12-14 12:36:17 +0200

    Applicationpath eklendi

---
+++ debana/debana.web/src/main/java/com/eteration/bootcamp/debana/rest/DebanaApplication.java
@@ -0,0 +1,9 @@
+package com.eteration.bootcamp.debana.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class DebanaApplication extends Application {
+
+}
